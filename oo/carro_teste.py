96b43981bf60ffefc90f98a4d9c69803b36fc488
jose <jrv.2008@hotmail.com>
2020-01-12 16:57:55 -0300

    Implementada Direção

--- oo/carro_teste.py
+++ oo/carro_teste.py
@@ -93,12 +93,36 @@ os seguintes atributos:
     'Norte'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_direcao()
-    'Oeste'"""
+    'Oeste'
+    """
 
 NORTE='Norte'
 SUL='Sul'
 LESTE='Leste'
 OESTE='Oeste'
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
 
 class Motor:
     def __init__(self):
@@ -113,7 +137,7 @@ class Motor:
 
 class Direcao():
 
-    rotacao_a_direita_dct={NORTE:LESTE, LESTE:SUL, SUL:OESTE, OESTE:NORTE}
+    rotacao_a_direita_dct =  {NORTE:LESTE,  LESTE:SUL,   SUL:OESTE,   OESTE:NORTE}
     rotacao_a_esquerda_dct = {NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL}
 
     def __init__(self):
